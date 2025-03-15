d2633bd1fd797bb2ed299aacf37f92ae06074a21
beam4869 <hongxuan@umich.edu>
2025-03-15 15:12:35 -0400

    Hongxuan's commit

--- hello_example.py
+++ hello_example.py
@@ -1,5 +1,10 @@
 def hello():
     print('hello, thank you, thank you very much')
+    
+def areyouOk():
+    print("Are you Ok? ")
+    hello()
 
 if __name__ == "__main__":
     hello()
+    areyouOk()
